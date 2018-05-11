8b0234cee737a8419ad7e750f2928397a1a93a40
Rawat Dinesh <dinesh.rawat@scheduleonce.com>
2018-05-11 15:13:14 +0530

    If env is development load static html without auth If env is production load static html with auth

--- app.ts
+++ app.ts
@@ -1,14 +1,20 @@
 const express = require('express');
 const basicAuth = require('express-basic-auth');
 const app = express();
-app.use(express.static('storybook-static'));
+const env = app.get('env');
 
-if(app.get('env') === 'production') {
+// If env is production load static html with auth
+if(env === 'production') {
     app.use(basicAuth({
-        users: {
-            'app': '#Udaan@SO!23'
-        },
+        users: {'app': '#Udaan@SO!23'},
         challenge: true
     }));
+    app.use(express.static('storybook-static'));
 }
+
+// If env is development load static html without auth
+if(env === 'development') {
+    app.use(express.static('storybook-static'));
+}
+
 app.listen(3000, () => console.log('Started listening to port:3000'));
