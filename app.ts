7afcc94fadf9fbd135732f2d70bd420df5d53e68
Michael <mderazon@gmail.com>
2018-05-15 11:51:37 +0300

    add test endpoint

--- app.ts
+++ app.ts
@@ -1,20 +1,23 @@
 const express = require('express');
 const basicAuth = require('express-basic-auth');
 const app = express();
-const env = app.get('env');
 
 // If env is production load static html with auth
-if(env === 'production') {
-    app.use(basicAuth({
-        users: {'app': '#Udaan@SO!23'},
-        challenge: true
-    }));
-    app.use(express.static('storybook-static'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(
+    basicAuth({
+      users: { app: '#Udaan@SO!23' },
+      challenge: true
+    })
+  );
 }
 
-// If env is development load static html without auth
-if(env === 'development') {
-    app.use(express.static('storybook-static'));
-}
+app.use(express.static('storybook-static'));
 
-app.listen(process.env.port, () => console.log('Started listening to port:3000'));
+app.get('/hello', (req, res) => {
+  res.status(200).send('Hello');
+});
+
+app.listen(process.env.port, () => {
+  console.log('Started listening on port', process.env.port);
+});
