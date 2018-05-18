dbbe124b00518161bf7d823982bc336bf3e62e43
Rawat Dinesh <dinesh.rawat@scheduleonce.com>
2018-05-18 11:17:14 +0530

    Added introduction story

---
+++ stories/introduction/introduction.component.ts
@@ -0,0 +1,11 @@
+import {
+    Component,
+    ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'storybook-introduction',
+    templateUrl: 'storybook-introduction.component.html',
+    encapsulation: ViewEncapsulation.None
+})
+export class IntroductionComponent {
+}
