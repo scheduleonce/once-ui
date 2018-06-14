d38b6c3891abc1f75816a5976d865972b15b49a9
Rawat Dinesh <dinesh.rawat@scheduleonce.com>
2018-06-14 18:10:19 +0530

    Updated dialog component

--- lib/dialog/src/content/dialog.content.component.ts
+++ lib/dialog/src/content/dialog.content.component.ts
@@ -1,6 +1,6 @@
 import {
-    Component,
-    Input
+  Component,
+  Input
 } from '@angular/core';
 import {OnceDialogConfig} from '../dialog-config';
 
@@ -8,15 +8,12 @@ import {OnceDialogConfig} from '../dialog-config';
  * Header component that wraps content section
  */
 @Component({
-    selector: 'once-ui-dialog-content',
-    templateUrl: 'dialog.content.component.html',
-    host: {
-        'class': 'onceUiDialogContent'
-    }
+  selector: 'once-ui-dialog-content',
+  templateUrl: 'dialog.content.component.html',
+  host: {
+    'class': 'onceUiDialogContent'
+  }
 })
 export class DialogContentComponent {
-    @Input() contentSettings: string;
-
-    constructor(public _config: OnceDialogConfig) {
-    }
+  @Input() content: string;
 }
