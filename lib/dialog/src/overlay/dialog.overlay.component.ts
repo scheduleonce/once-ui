f22aac3d6338d781615b494dc68a3e064aaf5784
Rawat Dinesh <dinesh.rawat@scheduleonce.com>
2018-06-19 15:11:05 +0530

    Update dialog box

--- lib/dialog/src/overlay/dialog.overlay.component.ts
+++ lib/dialog/src/overlay/dialog.overlay.component.ts
@@ -1,8 +1,9 @@
 import {
   Component,
-  Input
+  Input, HostListener, Inject
 } from '@angular/core';
 import {DialogService} from '../dialog.service';
+import {DOCUMENT} from "@angular/platform-browser";
 
 /**
  * Header component that wraps header section
@@ -16,15 +17,29 @@ import {DialogService} from '../dialog.service';
 })
 export class DialogOverlayComponent {
   @Input() modal: boolean;
+  @Input() escape: boolean;
 
-  constructor(public dialog: DialogService) {
+  constructor(public dialog: DialogService, @Inject(DOCUMENT) private document) {
+  }
+
+  /**
+   * On escape close
+   * @param event
+   */
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.keyCode === 27) {
+      if (this.escape) {
+        this.dialog.close();
+      }
+    }
   }
 
   /**
    * Close event from header
    */
   close() {
-    if(!this.modal) {
+    if (!this.modal) {
       this.dialog.close();
     }
   }
