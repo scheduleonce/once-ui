fbbc7fb226231298f66645c3bd3e22b84f39428b
Vikash Dahiya <vikash.dahiya@scheduleonce.com>
2018-10-15 15:05:55 +0530

    deleted previous project

--- .storybook/addons.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
-
-import '@storybook/addon-actions/register';
-import 'storybook-readme/register';
-import '@storybook/addon-knobs/register'
-import '@storybook/addon-options/register';
--- public-api.ts
+++
@@ -1,8 +0,0 @@
-/**
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
-export * from './lib/drop-down/index';
-export * from './lib/datepicker/index';
-export * from './lib/dialog/index';
