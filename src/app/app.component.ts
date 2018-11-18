dd31f52df9012f61fe6e05b45b0fad766eebf96b
Vikash Dahiya <vikash.dahiya@scheduleonce.com>
2018-11-18 16:46:27 +0530

    folder restructure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { OuiProgressButton } from './button/button';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('progressButton') progressButton: OuiProgressButton;
+  @ViewChild('progressLinkButton') progressLinkButton: OuiProgressButton;
+  @ViewChild('progressGhostButton') progressGhostButton: OuiProgressButton;
   title = 'ui-components';
+
+
+  progressButtonClick(){
+    this.progressButton.setToProgress();
+    setTimeout(()=>{
+      this.progressButton.setToDone();
+    },500);
+  }
+
+  progressButtonLinkClick(){
+    this.progressLinkButton.setToProgress();
+    setTimeout(()=>{
+      this.progressLinkButton.setToDone();
+    },500);
+  }
+
+  progressButtonGhostClick(){
+    this.progressGhostButton.setToProgress();
+    setTimeout(()=>{
+      this.progressGhostButton.setToDone();
+    },500);
+  }
 }
