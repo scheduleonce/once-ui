5ac7eddd713656facff44f995d0a1da81aec4f86
Garvit Rajput <garvit.rajput@scheduleonce.com>
2018-12-21 19:50:25 +0530

    slide toggle functionality added
    Former-commit-id: 9b72e4588c71ae0f1c612398be387d0dfa276994

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { OuiButtonModule } from './button/button-module';
 import { OuiDialogModule } from './dialog/dialog-module';
+import { OuiSlideToggleModule } from './slide-toggle/public-api';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, OuiButtonModule, OuiDialogModule],
+  imports: [BrowserModule, OuiButtonModule, OuiDialogModule,OuiSlideToggleModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/slide-toggle/slide-toggle-module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ObserversModule } from '@angular/cdk/observers';
+import { OuiSlideToggle } from './slide-toggle';
+
+@NgModule({
+  declarations: [OuiSlideToggle],
+  imports: [CommonModule, ObserversModule],
+  exports: [OuiSlideToggle]
+})
+export class OuiSlideToggleModule {}
