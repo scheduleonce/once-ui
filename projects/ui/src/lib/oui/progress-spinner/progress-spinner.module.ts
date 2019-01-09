2f65dc60d9ed1a10cbb38bbdc11ff96f759db0b0
sumitsharma22 <sumit.sharma@scheduleonce.com>
2019-01-09 11:24:18 +0530

    Removal of HttpClientModule from ProgressSpinner Module

--- projects/ui/src/lib/oui/progress-spinner/progress-spinner.module.ts
+++ projects/ui/src/lib/oui/progress-spinner/progress-spinner.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { OuiProgressSpinner } from './progress-spinner';
 
 @NgModule({
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule],
   declarations: [OuiProgressSpinner],
   exports: [OuiProgressSpinner]
 })
