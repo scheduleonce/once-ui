4ed895980cd23b3f1318c212140147b95cb3111b
Vikash Dahiya <vikash.dahiya@scheduleonce.com>
2019-01-29 19:17:19 +0530

    storybook changes
    Former-commit-id: 5cc32cf4e316b6e5299161cff8a9ab9451da1575

--- src/stories/table/index.stories.ts
+++ src/stories/table/index.stories.ts
@@ -86,22 +86,24 @@ const TABLEDATASOURCE = Array.from({ length: 100 }, (_, k) =>
       oui-input
       placeholder="Filter"
     />
-    <table oui-table #table [dataSource]="dataSource" ouiSort>
-      <ng-container
-        *ngFor="let column of displayedColumns"
-        ouiColumnDef="{{ column }}"
-      >
-        <th oui-header-cell *ouiHeaderCellDef oui-sort-header>
-          {{ column.toUpperCase() }}
-        </th>
-        <td oui-cell *ouiCellDef="let element">{{ element[column] }}</td>
-      </ng-container>
+    <div class="table-container">
+      <table oui-table #table [dataSource]="dataSource" ouiSort>
+        <ng-container
+          *ngFor="let column of displayedColumns"
+          ouiColumnDef="{{ column }}"
+        >
+          <th oui-header-cell *ouiHeaderCellDef oui-sort-header>
+            {{ column.toUpperCase() }}
+          </th>
+          <td oui-cell *ouiCellDef="let element">{{ element[column] }}</td>
+        </ng-container>
 
-      <tr oui-header-row *ouiHeaderRowDef="displayedColumns"></tr>
-      <tr oui-row *ouiRowDef="let row; columns: displayedColumns"></tr>
-    </table>
+        <tr oui-header-row *ouiHeaderRowDef="displayedColumns"></tr>
+        <tr oui-row *ouiRowDef="let row; columns: displayedColumns"></tr>
+      </table>
 
-    <oui-paginator pageSize="{{ pageSize }}"></oui-paginator>
+      <oui-paginator pageSize="{{ pageSize }}"></oui-paginator>
+    </div>
   `
 })
 export class OuiTableStorybook implements OnInit {
@@ -166,67 +168,71 @@ const USERINFODATASOURCE = [
 @Component({
   selector: 'oui-table-custom-storybook',
   template: `
-    <table oui-table [dataSource]="userInfoDataSource">
-      <ng-container ouiColumnDef="Users">
-        <th oui-header-cell *ouiHeaderCellDef class="col-one-width">Users</th>
-        <td oui-cell *ouiCellDef="let user">
-          <div class="ul-name clearfloat">
-            <div class="u-img"><img [src]="user.image" /></div>
-            <div class="usersName">
-              <div class="username-text">
-                <span>{{ user.name }}</span>
-              </div>
-              <div class="useremail-text">
-                <span [title]="user.email">{{ user.email }}</span>
+    <div class="table-container">
+      <table oui-table [dataSource]="userInfoDataSource">
+        <ng-container ouiColumnDef="Users">
+          <th oui-header-cell *ouiHeaderCellDef class="col-one-width">Users</th>
+          <td oui-cell *ouiCellDef="let user">
+            <div class="ul-name clearfloat">
+              <div class="u-img"><img [src]="user.image" /></div>
+              <div class="usersName">
+                <div class="username-text">
+                  <span>{{ user.name }}</span>
+                </div>
+                <div class="useremail-text">
+                  <span [title]="user.email">{{ user.email }}</span>
+                </div>
               </div>
             </div>
-          </div>
-        </td>
-      </ng-container>
+          </td>
+        </ng-container>
 
-      <ng-container ouiColumnDef="Role">
-        <th oui-header-cell *ouiHeaderCellDef class="col-two-width">Role</th>
-        <td oui-cell *ouiCellDef="let element">{{ element.role }}</td>
-      </ng-container>
+        <ng-container ouiColumnDef="Role">
+          <th oui-header-cell *ouiHeaderCellDef class="col-two-width">Role</th>
+          <td oui-cell *ouiCellDef="let element">{{ element.role }}</td>
+        </ng-container>
 
-      <ng-container ouiColumnDef="Integrations">
-        <th oui-header-cell *ouiHeaderCellDef class="col-three-width">
-          Integrations
-        </th>
-        <td oui-cell *ouiCellDef="let user">
-          <div class="integrationsContainer">
-            <ul>
-              <li *ngFor="let integration of user.integration">
-                <img [src]="INTEGRATIONS[integration]" />
-              </li>
-            </ul>
-          </div>
-        </td>
-      </ng-container>
+        <ng-container ouiColumnDef="Integrations">
+          <th oui-header-cell *ouiHeaderCellDef class="col-three-width">
+            Integrations
+          </th>
+          <td oui-cell *ouiCellDef="let user">
+            <div class="integrationsContainer">
+              <ul>
+                <li *ngFor="let integration of user.integration">
+                  <img [src]="INTEGRATIONS[integration]" />
+                </li>
+              </ul>
+            </div>
+          </td>
+        </ng-container>
 
-      <ng-container ouiColumnDef="Licenses">
-        <th oui-header-cell *ouiHeaderCellDef class="col-four-width">
-          Licenses
-        </th>
-        <td oui-cell *ouiCellDef="let user">
-          <div class="licensesContainer">
-            <ul>
-              <li *ngFor="let license of user.licence">
-                <img [src]="LICENCES[license]" />
-              </li>
-            </ul>
-          </div>
-        </td>
-      </ng-container>
+        <ng-container ouiColumnDef="Licenses">
+          <th oui-header-cell *ouiHeaderCellDef class="col-four-width">
+            Licenses
+          </th>
+          <td oui-cell *ouiCellDef="let user">
+            <div class="licensesContainer">
+              <ul>
+                <li *ngFor="let license of user.licence">
+                  <img [src]="LICENCES[license]" />
+                </li>
+              </ul>
+            </div>
+          </td>
+        </ng-container>
 
-      <ng-container ouiColumnDef="Status">
-        <th oui-header-cell *ouiHeaderCellDef class="col-five-width">Status</th>
-        <td oui-cell *ouiCellDef="let element">{{ element.status }}</td>
-      </ng-container>
+        <ng-container ouiColumnDef="Status">
+          <th oui-header-cell *ouiHeaderCellDef class="col-five-width">
+            Status
+          </th>
+          <td oui-cell *ouiCellDef="let element">{{ element.status }}</td>
+        </ng-container>
 
-      <tr oui-header-row *ouiHeaderRowDef="userInfoColumns"></tr>
-      <tr oui-row *ouiRowDef="let row; columns: userInfoColumns"></tr>
-    </table>
+        <tr oui-header-row *ouiHeaderRowDef="userInfoColumns"></tr>
+        <tr oui-row *ouiRowDef="let row; columns: userInfoColumns"></tr>
+      </table>
+    </div>
   `
 })
 export class OuiTableCustomStorybook implements OnInit {
