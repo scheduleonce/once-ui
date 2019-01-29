7aabba8f98e2f7ed21244267f09de8c676ba8357
Garvit Rajput <garvit.rajput@scheduleonce.com>
2019-01-29 15:54:35 +0530

    custom table added
    Former-commit-id: 1c656eec0f4232cda9073c5f36da089ac3d21965

--- src/stories/table/index.stories.ts
+++ src/stories/table/index.stories.ts
@@ -13,6 +13,13 @@ import { object, number } from '@storybook/addon-knobs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import markdownText from '../../../projects/ui/src/lib/oui/table/README.md';
+
+import paypal from '../../assets/images/paypal.svg';
+import zapier from '../../assets/images/zapier.svg';
+import google_calender from '../../assets/images/google-calender.svg';
+import salesforce from '../../assets/images/salesforce.svg';
+import scheduleonce from '../../assets/images/scheduleonce.svg';
+import inviteonce from '../../assets/images/inviteonce.svg';
 const NAMES: string[] = [
   'Maia',
   'Asher',
@@ -116,32 +123,189 @@ export class OuiTableStorybook implements OnInit {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
+
+const USERINFODATASOURCE = [
+  {
+    name: 'anurag',
+    email: 'anurag+122333223423523523.scheduleonce@gmail.com',
+    role: 'member',
+    image: '',
+    integration: ['zapier', 'paypal', 'google-calender', 'salesforce'],
+    licence: ['scheduleonce', 'inviteonce'],
+    status: 'invited'
+  },
+  {
+    name: 'anurag',
+    email: 'anurag.scheduleonce@gmail.com',
+    role: 'member',
+    image: '',
+    integration: ['zapier', 'paypal', 'google-calender', 'salesforce'],
+    licence: ['scheduleonce', 'inviteonce'],
+    status: 'invited'
+  },
+  {
+    name: 'anurag',
+    email: 'anurag.scheduleonce@gmail.com',
+    role: 'member',
+    image: '',
+    integration: ['zapier', 'paypal', 'google-calender', 'salesforce'],
+    licence: ['scheduleonce', 'inviteonce'],
+    status: 'invited'
+  },
+  {
+    name: 'anurag',
+    email: 'anurag.scheduleonce@gmail.com',
+    role: 'member',
+    image: '',
+    integration: ['zapier', 'paypal', 'google-calender', 'salesforce'],
+    licence: ['scheduleonce', 'inviteonce'],
+    status: 'invited'
+  }
+];
+
+@Component({
+  selector: 'oui-table-custom-storybook',
+  template: `
+    <table oui-table [dataSource]="userInfoDataSource">
+      <ng-container ouiColumnDef="Users">
+        <th oui-header-cell *ouiHeaderCellDef class="col-one-width">Users</th>
+        <td oui-cell *ouiCellDef="let user">
+          <div class="ul-name clearfloat">
+            <div class="u-img"><img [src]="user.image" /></div>
+            <div class="usersName">
+              <div class="username-text">
+                <span>{{ user.name }}</span>
+              </div>
+              <div class="useremail-text">
+                <span [title]="user.email">{{ user.email }}</span>
+              </div>
+            </div>
+          </div>
+        </td>
+      </ng-container>
+
+      <ng-container ouiColumnDef="Role">
+        <th oui-header-cell *ouiHeaderCellDef class="col-two-width">Role</th>
+        <td oui-cell *ouiCellDef="let element">{{ element.role }}</td>
+      </ng-container>
+
+      <ng-container ouiColumnDef="Integrations">
+        <th oui-header-cell *ouiHeaderCellDef class="col-three-width">
+          Integrations
+        </th>
+        <td oui-cell *ouiCellDef="let user">
+          <div class="integrationsContainer">
+            <ul>
+              <li *ngFor="let integration of user.integration">
+                <img [src]="INTEGRATIONS[integration]" />
+              </li>
+            </ul>
+          </div>
+        </td>
+      </ng-container>
+
+      <ng-container ouiColumnDef="Licenses">
+        <th oui-header-cell *ouiHeaderCellDef class="col-four-width">
+          Licenses
+        </th>
+        <td oui-cell *ouiCellDef="let user">
+          <div class="licensesContainer">
+            <ul>
+              <li *ngFor="let license of user.licence">
+                <img [src]="LICENCES[license]" />
+              </li>
+            </ul>
+          </div>
+        </td>
+      </ng-container>
+
+      <ng-container ouiColumnDef="Status">
+        <th oui-header-cell *ouiHeaderCellDef class="col-five-width">Status</th>
+        <td oui-cell *ouiCellDef="let element">{{ element.status }}</td>
+      </ng-container>
+
+      <tr oui-header-row *ouiHeaderRowDef="userInfoColumns"></tr>
+      <tr oui-row *ouiRowDef="let row; columns: userInfoColumns"></tr>
+    </table>
+  `
+})
+export class OuiTableCustomStorybook implements OnInit {
+  INTEGRATIONS = {
+    paypal: paypal,
+    zapier: zapier,
+    'google-calender': google_calender,
+    salesforce: salesforce
+  };
+  LICENCES = {
+    scheduleonce: scheduleonce,
+    inviteonce: inviteonce
+  };
+  @ViewChild(OuiSort) sort: OuiSort;
+  userInfoColumns: string[] = [
+    'Users',
+    'Role',
+    'Integrations',
+    'Licenses',
+    'Status'
+  ];
+  @Input() users: any[] = [];
+  @Input() pageSize: any[] = [];
+  userInfoDataSource = new OuiTableDataSource(this.users);
+  constructor() {}
+  ngOnInit() {
+    this.userInfoDataSource = new OuiTableDataSource(this.users);
+    this.userInfoDataSource.sort = this.sort;
+  }
+}
+
 const valueOptions = {
   range: true,
   min: 5,
   max: 50,
   step: 5
 };
-storiesOf('Table', module).add(
-  'default',
-  () => ({
-    moduleMetadata: {
-      imports: [
-        OuiTableModule,
-        OuiSortModule,
-        OuiPaginatorModule,
-        OuiIconModule,
-        OuiInputModule,
-        BrowserAnimationsModule
-      ],
-      schemas: [],
-      declarations: [OuiTableStorybook]
-    },
-    template: `<oui-table-storybook [pageSize]="pageSize" [users]="data"></oui-table-storybook>`,
-    props: {
-      pageSize: number('pageSize', 10, valueOptions),
-      data: object('data', TABLEDATASOURCE)
-    }
-  }),
-  { notes: { markdown: markdownText } }
-);
+storiesOf('Table', module)
+  .add(
+    'default',
+    () => ({
+      moduleMetadata: {
+        imports: [
+          OuiTableModule,
+          OuiSortModule,
+          OuiPaginatorModule,
+          OuiIconModule,
+          OuiInputModule,
+          BrowserAnimationsModule
+        ],
+        schemas: [],
+        declarations: [OuiTableStorybook]
+      },
+      template: `<oui-table-storybook [pageSize]="pageSize" [users]="data"></oui-table-storybook>`,
+      props: {
+        pageSize: number('pageSize', 10, valueOptions),
+        data: object('data', TABLEDATASOURCE)
+      }
+    }),
+    { notes: { markdown: markdownText } }
+  )
+  .add(
+    'custom',
+    () => ({
+      moduleMetadata: {
+        imports: [
+          OuiTableModule,
+          OuiSortModule,
+          OuiIconModule,
+          OuiInputModule,
+          BrowserAnimationsModule
+        ],
+        schemas: [],
+        declarations: [OuiTableCustomStorybook]
+      },
+      template: `<oui-table-custom-storybook [users]="data"></oui-table-custom-storybook>`,
+      props: {
+        data: object('data', USERINFODATASOURCE)
+      }
+    }),
+    { notes: { markdown: markdownText } }
+  );
