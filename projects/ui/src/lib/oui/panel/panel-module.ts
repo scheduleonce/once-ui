007250cf9a8e69a3709e4ed8f3d0122f3b712464
Vikash Dahiya <vikash.dahiya@scheduleonce.com>
2019-02-22 18:32:42 +0530

    oui-panel-icon added
    Former-commit-id: 6ea106286b37b58d2bb3c3c4f5639a122ffd32e4

--- projects/ui/src/lib/oui/panel/panel-module.ts
+++ projects/ui/src/lib/oui/panel/panel-module.ts
@@ -2,7 +2,7 @@ import { OverlayModule } from '@angular/cdk/overlay';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { OuiPanelContent } from './panel-content';
-import { OuiPanel } from './panel';
+import { OuiPanel, OuiPanelIcon } from './panel';
 import {
   OuiPanelTrigger,
   OUI_PANEL_SCROLL_STRATEGY_FACTORY_PROVIDER
@@ -10,8 +10,8 @@ import {
 
 @NgModule({
   imports: [CommonModule, OverlayModule],
-  exports: [OuiPanel, OuiPanelTrigger, OuiPanelContent],
-  declarations: [OuiPanel, OuiPanelTrigger, OuiPanelContent],
+  exports: [OuiPanel, OuiPanelTrigger, OuiPanelContent, OuiPanelIcon],
+  declarations: [OuiPanel, OuiPanelTrigger, OuiPanelContent, OuiPanelIcon],
   providers: [OUI_PANEL_SCROLL_STRATEGY_FACTORY_PROVIDER]
 })
 export class OuiPanelModule {}
