542ee088ecb02a8154abe411ce94c9261e91fba8
Garvit Rajput <garvit.rajput@scheduleonce.com>
2019-02-25 15:04:44 +0530

    initial setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var del = require('del');
 
 gulp.task('copy', function() {
   return gulp.src(['./projects/ui/**/*.scss'], {}).pipe(gulp.dest('./dist/ui'));
+});
+gulp.task('copy-storybook-config', function() {
+  return gulp.src(['./static.json'], {}).pipe(gulp.dest('./storybook-static'));
+});
+gulp.task('clean-storybook-build', function() {
+  return del(['./storybook-static/**/*']);
 });
