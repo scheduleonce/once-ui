a7a907e06486be698e53240b1cd13eb49ff34ebc
Garvit Rajput <garvit.rajput@scheduleonce.com>
2019-07-09 16:47:44 +0530

    accessiblity addon removed
    Former-commit-id: 6ad33c7c75a36b3ca42a9981db8d2004e3c7c32c

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -3,4 +3,3 @@ import '@storybook/addon-knobs/register';
 import '@storybook/addon-options/register';
 import '@storybook/addon-notes/register';
 import '@storybook/addon-viewport/register';
-import '@storybook/addon-a11y/register';
