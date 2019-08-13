8629938f1233573f33861a75b7b234e4e5b4ad68
sumitsharma22 <sumit.sharma@scheduleonce.com>
2019-08-13 13:21:29 +0530

    icon added by the icon set

--- projects/ui/src/lib/oui/input/input.ts
+++ projects/ui/src/lib/oui/input/input.ts
@@ -25,6 +25,7 @@ import {
   CanUpdateErrorStateCtor,
   mixinErrorState
 } from '../core/common-behaviors/error-state';
+import { ICONS } from '../core/shared/icons';
 
 // Invalid input type. Using one of these will throw an OuiInputUnsupportedTypeError.
 const OUI_INPUT_INVALID_TYPES = [
@@ -38,14 +39,6 @@ const OUI_INPUT_INVALID_TYPES = [
   'reset',
   'submit'
 ];
-
-const OUI_NUMBER_ARROW = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="12px" height="8px" viewBox="0 0 12 8" version="1.1">
-<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-    <g id="DropDown" transform="translate(-527.000000, -179.000000)" fill="#4A4A4A">
-        <path d="M530.684211,183.315789 L537,183.315789 L537,185 L530.684211,185 L529,185 L529,177 L530.684211,177 L530.684211,183.315789 Z" id="Combined-Shape-Copy-3" transform="translate(533.000000, 181.000000) scale(-1, -1) rotate(-225.000000) translate(-533.000000, -181.000000) "/>
-    </g>
-</g>
-</svg>`;
 
 /** Default color palette for input */
 const DEFAULT_COLOR = 'primary';
@@ -369,7 +362,7 @@ export class OuiInput extends _OuiInputMixinBase
               onclick="(this.parentElement.previousSibling.value=parseInt(this.parentElement.previousSibling.value?this.parentElement.previousSibling.value:0)${curr}1); this.parentElement.previousSibling.dispatchEvent(new Event('change'))"
               class="quantity-button quantity-${
                 curr === '-' ? 'down' : 'up'
-              }">${OUI_NUMBER_ARROW}</div>`
+              }">${ICONS.DOWN_ARROW_8x8}</div>`
         );
       }, '');
       this._elementRef.nativeElement.parentNode.insertBefore(
