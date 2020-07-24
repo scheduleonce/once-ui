a01fe3b376d1994fb13559cb59531f7fdf925c75
Sumit Sheokand <sumit.sheokand@oncehub.com>
2020-07-24 17:24:05 +0530

    alignment

--- ui/src/stories/select/index.stories.ts
+++ ui/src/stories/select/index.stories.ts
@@ -70,7 +70,7 @@ storiesOf('Form Field/Select', module)
     <div style="width: 213px;">
     <oui-form-field [appearance]="appearance">
         <oui-select (selectionChange)="onChange($event)" [large]="large" [placeholder]="placeholder" multiple [disabled]="disabled">
-         <oui-option *ngFor="let food of foods" [value]="food">
+          <oui-option *ngFor="let food of foods" [value]="food">
             {{food}}
           </oui-option>
         </oui-select>
