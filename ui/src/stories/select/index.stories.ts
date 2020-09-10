ef80e51e7a2d37b791aa0ef755abc27ba10bd4f8
Sumit Sheokand <sumit.sheokand@oncehub.com>
2020-09-10 18:48:53 +0530

    demo code

--- ui/src/stories/select/index.stories.ts
+++ ui/src/stories/select/index.stories.ts
@@ -61,13 +61,16 @@ export const Multi = () => ({
   template: `
     <div style="width: 213px;">
     <oui-form-field [appearance]="appearance">
-        <oui-select (selectionChange)="onChange($event)" [large]="large" [placeholder]="placeholder" multiple [disabled]="disabled">
-          <oui-option *ngFor="let food of foods" [value]="food">
+        <oui-select (selectionChange)="onChange($event)" [large]="large" [(ngModel)]="currentValue" [placeholder]="placeholder" multiple [disabled]="disabled">
+        <oui-select-search [(ngModel)]="keyword"></oui-select-search>
+        <oui-option *ngFor="let food of (foods | filterOptions: keyword)" [value]="food">
             {{food}}
           </oui-option>
         </oui-select>
       </oui-form-field>
     </div>
+    <br><br><br><br><br><br>
+    <button (click)="clearCurrentValue()">Clear</button>
     `,
   props: {
     foods: array('foods', ['Pizza', 'Burgers', 'Steak', 'Tacos']),
@@ -75,6 +78,10 @@ export const Multi = () => ({
     disabled: boolean('disabled', false),
     large: boolean('large', false),
     onChange: action('change'),
+    currentValue:["Pizza"],
+    clearCurrentValue : function(){
+      this.currentValue = [];
+    },
     appearance: select('appearance', APPEARANCE, APPEARANCE[0]),
   },
   moduleMetadata: {
