47a6c5d8be6e3ee2def2cdf9926209af4f70f79b
Sumit Sharma <sumit.sharma@oncehub.com>
2020-10-08 12:40:42 +0530

    Created a mdx format storybook POC

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,10 @@
 module.exports = {
-  stories: ['../ui/src/stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../ui/src/stories/**/*.stories.@(js|jsx|ts|mdx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-knobs',
     '@storybook/addon-controls',
+    '@storybook/addon-docs'
   ],
 };
