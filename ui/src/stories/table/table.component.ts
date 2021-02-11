9214619ff6914d1b5e4a2cdb0001f4c7916f327f
Naveen Bhushan <naveen.bhushan@oncehub.com>
2021-02-11 13:04:39 +0530

    Undo commit on this branch

--- ui/src/stories/table/table.component.ts
+++ ui/src/stories/table/table.component.ts
@@ -35,31 +35,9 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class OuiTableStorybook implements OnInit, OnChanges {
   @ViewChild(OuiSort, { static: true }) sort: OuiSort;
   @ViewChild(OuiPaginator, { static: true }) paginator: OuiPaginator;
-  // displayedColumns: string[] = [];
-  @Input() users: any[] = [
-    { position: 1, name: "Hydrogen", weight: 1.0079, symbol: "H" },
-    { position: 2, name: "Helium", weight: 4.0026, symbol: "He" },
-    { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
-    { position: 4, name: "Beryllium", weight: 9.0122, symbol: "Be" },
-    { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
-    { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
-    { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
-    { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
-    { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
-    { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" },
-    { position: 1, name: "Hydrogen", weight: 1.0079, symbol: "H" },
-    { position: 2, name: "Helium", weight: 4.0026, symbol: "He" },
-    { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
-    { position: 4, name: "Beryllium", weight: 9.0122, symbol: "Be" },
-    { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
-    { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
-    { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
-    { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
-    { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
-    { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" }
-  ]
-  @Input() pageSize: any[] = [];
   displayedColumns: string[] = [];
+  @Input() users: any[] = [];
+  @Input() pageSize: any[] = [];
   dataSource = new OuiTableDataSource(this.users);
   constructor() {}
 
