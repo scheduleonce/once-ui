ea1bab721eed9928444533cb925b79e9052ea538
Sumit Sharma <sumit.sharma@oncehub.com>
2021-02-16 11:42:24 +0530

    Prettier fixes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "stories": [
-    "../ui/src/stories/**/*.stories.mdx",
-    "../ui/src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../ui/src/stories/**/*.stories.mdx',
+    '../ui/src/stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-docs',
