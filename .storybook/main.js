cf9fd7ecb6750987ba6076c658e1ac5ae99cca9a
sumitsharma22 <sumit.sharma@scheduleonce.com>
2021-09-21 11:35:34 +0530

    Storybook Fixes Updated with stable version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,14 @@
 module.exports = {
-  stories: [
-    '../ui/src/stories/**/*.stories.mdx',
-    '../ui/src/stories/**/*.stories.@(js|jsx|ts|tsx)',
+  "stories": [
+    "../ui/src/stories/**/*.stories.mdx",
+    "../ui/src/stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  addons: [
-    '@storybook/addon-docs',
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-knobs',
-    '@storybook/addon-controls',
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-knobs"
   ],
-};
+  "core": {
+    "builder": "webpack5"
+  }
+}
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,16 @@
-import { setCompodocJson } from '@storybook/addon-docs/angular';
-import docJson from '../documentation.json';
+import { setCompodocJson } from "@storybook/addon-docs/angular";
+import docJson from "../documentation.json";
 import '!style-loader!css-loader!../ui/src/stories/styles/style.css';
 import '!style-loader!css-loader!../ui/src/stories/styles/themes/oncehub.css';
-
 setCompodocJson(docJson);
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  // layout: 'centered',
-};
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: { inlineStories: true },
+}
