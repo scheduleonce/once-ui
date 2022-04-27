e4d7c5ebe55205b70cd1e33979bae8166c137761
SoniRani <soni.rani@scheduleonce.com>
2022-04-27 13:22:15 +0530

    Revert "https://scheduleonce.atlassian.net/browse/ONCEHUB-53279"
    This reverts commit cf89e1dffd488d853569ea73d973d0724bd20a96.

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,6 @@ npm whoami
 npm i
 npm run package
 filename="$(npm pack --dry-run | tail -n 1)"
-echo "$1"
-
-if [[ "$1" == "qa" ]] || [[ "$1" == "master" ]] || [[ "$1" == "staging" ]] || [[ "$1" == "staging-app2" ]]
-then
-  npm publish dist/ui/$filename --registry=https://registry.npmjs.org/
-else
-  npm publish --tag beta dist/ui/$filename --registry=https://registry.npmjs.org/
-fi
+npm publish dist/ui/$filename --registry=https://registry.npmjs.org/
 
 echo "$filename package pushed to NPM successfully"
