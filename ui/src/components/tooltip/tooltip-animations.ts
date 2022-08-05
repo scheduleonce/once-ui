99daf01062adaee6b0a95ba8cdff18edece3310a
Vipin Dangwal <vipin.dangwal@oncehub.com>
2022-08-05 11:30:15 +0530

    OUI tooltip is not working correctly on the OnceUI pop up

--- ui/src/components/tooltip/tooltip-animations.ts
+++ ui/src/components/tooltip/tooltip-animations.ts
@@ -30,7 +30,7 @@ export const ouiTooltipAnimations: {
     ),
     transition(
       '* => hidden',
-      animate('100ms cubic-bezier(0, 0, 0.2, 1)', style({ opacity: 0 }))
+      animate('0ms cubic-bezier(0, 0, 0.2, 1)', style({ opacity: 0 }))
     ),
   ]),
 };
