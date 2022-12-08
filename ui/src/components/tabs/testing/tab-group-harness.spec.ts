15dbf26d14e6e58de2aaa26e4edad000b4f8f472
ShubhamSaxena <ShubhamSaxena@DESKTOP-6RJBF0C>
2022-12-08 16:52:48 +0530

    Removed testing files

--- ui/src/components/tabs/testing/tab-group-harness.spec.ts
+++
@@ -1,8 +0,0 @@
-import {MatTabsModule} from '@angular/material/tabs';
-import {runTabGroupHarnessTests} from './tab-group-shared.spec';
-import {MatTabGroupHarness} from './tab-group-harness';
-import {MatTabHarness} from './tab-harness';
-
-describe('MDC-based MatTabGroupHarness', () => {
-  runTabGroupHarnessTests(MatTabsModule, MatTabGroupHarness, MatTabHarness);
-});
