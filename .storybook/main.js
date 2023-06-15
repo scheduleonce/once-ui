4d6030ab581cf924fac7b47e896bdfddd23ea95d
siddharth <siddharth.shahi@oncehub.com>
2023-06-15 18:39:59 +0530

    update the storybook version and related packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,11 @@
 module.exports = {
-  stories: [
-    '../ui/src/stories/**/*.stories.mdx',
-    '../ui/src/stories/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: ['@storybook/addon-essentials'],
-  core: {
-    builder: 'webpack5',
+  stories: ['../ui/src/stories/**/*.stories.mdx', '../ui/src/stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-essentials', "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: '@storybook/angular',
+    options: {}
   },
+  docs: {
+    autodocs: true
+  }
 };
