858fd3107fb6e80e8dd18709b50030098cf92e76
Areeb2512 <areeb.alam@oncehub.com>
2023-08-16 16:11:26 +0530

    prettier fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,14 @@
 module.exports = {
-  stories: ['../ui/src/stories/**/*.stories.mdx', '../ui/src/stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', "@storybook/addon-mdx-gfm"],
+  stories: [
+    '../ui/src/stories/**/*.stories.mdx',
+    '../ui/src/stories/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-mdx-gfm'],
   framework: {
     name: '@storybook/angular',
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
