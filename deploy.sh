8b83f8f010f600c815187c445d6c5123dddbe773
Tanish Jain <83695026+OhTanishJain@users.noreply.github.com>
2024-03-06 11:15:07 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 set -e
 
-node package-deploy/npm-login.js $(cat "/etc/npm-cred/NPM_AUTH_TOKEN")
-npm whoami
+node package-deploy/npm-login.js $(cat "/etc/azure-artifact-cred/AZURE-ARTIFACT-AUTH-TOKEN")
+
 npm i
-chown root:root . && npm run package
-filename="$(npm pack --dry-run | tail -n 1)"
-echo "$1"
+npm config list
 
 if [[ "$1" == "qa" ]] || [[ "$1" == "master" ]] || [[ "$1" == "staging" ]] || [[ "$1" == "staging-app2" ]]
 then
-  npm publish dist/ui/$filename --registry=https://registry.npmjs.org/
+  npm publish --registry=https://pkgs.dev.azure.com/oncehub/pkg/_packaging/node_package/npm/registry/
 else
-  npm publish --tag beta dist/ui/$filename --registry=https://registry.npmjs.org/
+  npm publish --tag beta --registry=https://pkgs.dev.azure.com/oncehub/pkg/_packaging/node_package/npm/registry/
 fi
 
-echo "$filename package pushed to NPM successfully"
+echo "Package pushed to Azure successfully"
