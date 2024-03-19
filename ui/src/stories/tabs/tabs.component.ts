bcec4bb7538507e14688bad7b780d49fb65b3414
siddharth <siddharth.shahi@oncehub.com>
2024-03-19 20:52:59 +0530

    fixed the focus state and some css

--- ui/src/stories/tabs/tabs.component.ts
+++ ui/src/stories/tabs/tabs.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'oui-tab-storybook',
   template: `
-    <oui-tab-group oui-stretch-tabs="false">
+    <oui-tab-group oui-stretch-tabs="true">
       <oui-tab label="First1"><h2>Content in tab 1</h2></oui-tab>
       <oui-tab label="Second"><h2>Content in tab 2</h2></oui-tab>
       <oui-tab label="Third"><h2>Content in tab 3</h2></oui-tab>
