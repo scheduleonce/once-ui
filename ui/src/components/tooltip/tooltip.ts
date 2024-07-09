200fbaeff620987de468acd46856d9865ce6beb7
Faizal P.A <faizal.pa@oncehub.com>
2024-07-09 15:21:22 +0530

    remove console logs

--- ui/src/components/tooltip/tooltip.ts
+++ ui/src/components/tooltip/tooltip.ts
@@ -165,9 +165,7 @@ export class TooltipComponent {
    */
   show(): void {
     // Cancel the delayed hide if it is scheduled
-    console.log('show');
     if (this._hideTimeoutId) {
-      console.log('show timeout');
       clearTimeout(this._hideTimeoutId);
       this._hideTimeoutId = null;
     }
@@ -191,9 +189,7 @@ export class TooltipComponent {
    */
   hide(): void {
     // Cancel the delayed show if it is scheduled
-    console.log('hide');
     if (this._showTimeoutId) {
-      console.log('hide timeout');
       clearTimeout(this._showTimeoutId);
       this._showTimeoutId = null;
     }
@@ -217,12 +213,10 @@ export class TooltipComponent {
   }
 
   _animationStart() {
-    console.log('this is animationstart component');
     this._closeOnInteraction = false;
   }
 
   _animationDone(event: AnimationEvent): void {
-    console.log('this is animationDone component');
     const toState = event.toState as TooltipVisibility;
 
     if (toState === 'hidden' && !this.isVisible()) {
