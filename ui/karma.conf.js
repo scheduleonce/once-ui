f778d01129aff268e6b9b3523b0b4825ddcb2e43
khushigupta0625 <khushi.gupta@oncehub.com>
2026-06-18 17:20:58 +0530

    chore(deps): upgrade Node.js version to 24.16.0 in CI and Dockerfile; update Puppeteer to version 25.1.0 in package.json and package-lock.json

--- ui/karma.conf.js
+++ ui/karma.conf.js
@@ -1,6 +1,10 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-process.env.CHROME_BIN = require('puppeteer').executablePath();
+try {
+  process.env.CHROME_BIN = require('puppeteer').computeExecutablePath();
+} catch (e) {
+  process.env.CHROME_BIN = '/usr/bin/google-chrome-stable';
+}
 module.exports = function (config) {
   config.set({
     basePath: '',
